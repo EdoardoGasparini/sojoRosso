44d6b30d91fd15ea0647c9bdcfc6aac426492793
EdoardoGasparini <edo.gasparini@gmail.com>
2023-10-08 16:12:56 +0200

    header done + navigation setup

--- sojoWeb/src/app/app-routing.module.ts
+++ sojoWeb/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { EventComponent } from './event/event.component';
 
 const routes: Routes = [
-  {path:'', component: AboutComponent, pathMatch:'full'}
+  {path:'', component: AboutComponent, pathMatch:'full'},
+  {path:'eventi', component:EventComponent},
+  {path:'galleria', component:GalleryComponent},
+  {path:'contatti', component:ContactComponent}  
 ];
 
 @NgModule({
--- sojoWeb/src/app/app.module.ts
+++ sojoWeb/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { EventComponent } from './event/event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    GalleryComponent,
+    ContactComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
