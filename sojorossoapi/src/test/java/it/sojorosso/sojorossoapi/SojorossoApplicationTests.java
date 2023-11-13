917c74065a176b8fa8815684367613da977832fe
EdoardoGasparini <edo.gasparini@gmail.com>
2023-11-13 09:12:39 +0100

    init backend

---
+++ sojorossoapi/src/test/java/it/sojorosso/sojorossoapi/SojorossoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sojorosso.sojorossoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SojorossoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
