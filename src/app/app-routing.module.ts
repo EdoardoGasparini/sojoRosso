42d8cb60a44c0ca16b14887215b76adaff1f999e
EdoardoGasparini <edo.gasparini@gmail.com>
2024-05-12 13:12:32 +0200

    fine modifica header/about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,12 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
-import { ContactComponent } from './contact/contact.component';
 import { EventComponent } from './event/event.component';
-import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   {path:'', component: AboutComponent, pathMatch:'full'},
   {path:'eventi', component:EventComponent},
-  {path:'galleria', component:GalleryComponent},
-  {path:'contatti', component:ContactComponent},
-  {path:'pannello', component:AuthComponent}  
+  {path:'galleria', component:GalleryComponent},  
 ];
 
 @NgModule({
